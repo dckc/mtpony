e1666b2565210b6205f3b882219fd9c63ca28cf4
Dan Connolly <dckc@madmode.com>
2016-04-20 00:29:06 -0500

    render MAST as more idiomatic, readable sexpr

--- mastrd.rkt
+++ mastrd.rkt
@@ -39,15 +39,30 @@
   (define (next-exprs)
     (for/list [(ix (in-range (decode-int port)))] (next-expr)) )
   (define (next-named-exprs)
-    (for/list [(ix (in-range (decode-int port)))]
-      `(named-arg ,(next-expr) ,(next-expr)) ) )
+    (apply append
+           (for/list [(ix (in-range (decode-int port)))]
+             `(,(string->keyword (next-expr)) ,(next-expr)) ) ) )
   (define (next-patt)
     (list-ref patts (decode-int port)) )
   (define (next-patts)
     (for/list [(ix (in-range (decode-int port)))] (next-patt)) )
   (define (next-named-patts)
-    (for/list [(ix (in-range (decode-int port)))]
-      `(,(next-expr) => ,(next-expr) := ,(next-expr)) ) )
+    (apply append
+           (for/list [(ix (in-range (decode-int port)))]
+             (let ((kw (string->keyword (next-expr)))
+                   (patt (next-patt))
+                   (default (next-expr)))
+               (if (eq? 'not-present default)
+                   `(,kw ,patt)
+                   `(,kw [,patt ,default]))
+               ))))
+
+  (define (opt kw node)
+    (if (eq? node 'not-present) '() `(,(string->keyword kw) ,node)))
+  (define (opt-rep kw nodes)
+    (if (equal? nodes '()) '() `(,(string->keyword kw) ,@nodes)))
+  (define (opt-doc node)
+    (if (equal? node "") '() `(#:doc ,node)))
 
   (define (decode-pattern)
     (let ((ptag (tag (must-read-byte port))))
@@ -55,15 +70,15 @@
         [(#\A) (let ((expr (next-expr))
                      (patt (next-patt)))
                  `(via ,expr ,patt))]
-        [(#\F) (let ((name (decode-str port))
+        [(#\F) (let ((name (string->symbol (decode-str port)))
                      (guard (next-expr)))
-                 `(final ,name #:guard ,guard) )]
+                 `(final ,name ,@(opt "guard" guard)) )]
         [(#\I) (let ((guard (next-expr)))
-                 `(_ #:guard ,guard))]
-        [(#\L) `(list ,(next-patts))]
-        [(#\V) (let ((name (decode-str port))
+                 `(_ ,@(opt "guard" guard)) )]
+        [(#\L) (list->vector (next-patts))]
+        [(#\V) (let ((name (string->symbol (decode-str port)))
                      (guard (next-expr)))
-                 `(var ,name #:guard ,guard) )]
+                 `(var ,name ,@(opt "guard" guard)) )]
         [else (error (format "pattern tag??? ~v" ptag))]
         ) ) )
 
@@ -74,25 +89,24 @@
                        (expr (next-expr)))
                    (go-e `(assign ,target ,expr)))]
           [(#\C) (go-e (let ((target (next-expr))
-                             (verb (decode-str port)) ; symbol?
+                             (verb (string->symbol (decode-str port)))
                              (args (next-exprs))
                              (kwargs (next-named-exprs)))
-                         ; splice args and use #:x for kwargs
-                         `(send ,target ,verb ,args ,kwargs) )) ]
+                         `(send ,target ,verb ,@args ,@kwargs) )) ]
           [(#\D) (let ((patt (next-patt))
                        (exit (next-expr))
                        (expr (next-expr)))
-                   (go-e `(def ,patt #:exit ,exit ,expr)))]
+                   (go-e `(def ,patt ,@(opt "exit" exit) ,expr)))]
           [(#\L) (go-e (decode-literal port))]
           [(#\M) (let ((doc (decode-str port))
-                       (verb (decode-str port))
+                       (verb (string->symbol (decode-str port)))
                        (patts (next-patts))
                        (namedPatts (next-named-patts))
                        (guard (next-expr))
                        (block (next-expr)))
-                   (go-e `(define/method ,verb ,patts ,namedPatts
-                            #:guard ,guard
-                            #:doc ,doc
+                   (go-e `(define/method ,verb ,(append patts namedPatts)
+                            ,@(opt "guard" guard)
+                            ,@(opt-doc doc)
                             ,block)) )]
           [(#\O) (let ((doc (decode-str port))
                        (patt (next-patt))
@@ -101,12 +115,12 @@
                        (methods (next-exprs))
                        (matchers (next-exprs)))
                    (go-e `(define/object ,patt
-                            #:as ,asExpr
-                            #:implements ,implements
-                            #:doc ,doc
-                            ,methods
-                            #:matchers ,matchers)))]
-          [(#\N) (go-e `(noun ,(decode-str port)))]
+                            ,@(opt "as" asExpr)
+                            ,@(opt-rep "implements" implements)
+                            ,@(opt-doc doc)
+                            ,@methods
+                            ,@(opt-rep "matchers" matchers))))]
+          [(#\N) (go-e (string->symbol (decode-str port)))]
           [(#\P) (go-p (decode-pattern))]
           [(#\S) (go-e `(begin ,(next-exprs)))]
           [else (error (format "expr tag??? ~v ~v" b (integer->char b)))]
@@ -114,11 +128,11 @@
 
 (define (decode-literal port)
   (case (tag (must-read-byte port))
-    [(#\C) `(lit ,(read-char port))]
+    [(#\C) `(quote ,(read-char port))]
     [(#\D) (error 'TODO-double)]
-    [(#\I) `(lit ,(zz (decode-int port)))]
+    [(#\I) `(quote ,(zz (decode-int port)))]
     [(#\N) 'not-present]
-    [(#\S) `(lit ,(decode-str port))]
+    [(#\S) `(quote ,(decode-str port))]
     [else (error 'bad-literal-tag)]
      ) )
 
