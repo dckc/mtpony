73aaa61eea186cce10842a95376519192c95a2a3
Dan Connolly <dckc@madmode.com>
2017-05-28 14:59:56 -0500

    run_suite: use base json; make doctest a struct

--- run_suite.rkt
+++ run_suite.rkt
@@ -1,24 +1,33 @@
 #lang racket
 
+(require json)
 (require file/sha1)
-(require json-parsing)
 
 (require "mastrd.rkt")
 
-(define (run-test test-case)
-  (let* ([section (hash-ref test-case 'section)]
-         [lineno (hash-ref test-case 'lineno)]
-         [mast-hex (hash-ref test-case 'MAST)]
-         [mast-bytes (hex-string->bytes mast-hex)])
-    (call-with-input-bytes
-     mast-bytes
-     (lambda (mast-port)
-       (printf "~A~A: " section lineno)
-       (pretty-print (decode-mast mast-port))
-       ))))
+(struct doctest (source section lineno want MAST))
+(define (hash->doctest obj)
+  (doctest
+       (hash-ref obj 'source)
+       (hash-ref obj 'section)
+       (hash-ref obj 'lineno)
+       (hash-ref obj 'want)
+       (hex-string->bytes (hash-ref obj 'MAST))))
+
+(define monte-suite
+  (call-with-input-file "test/doctest_wm.json"
+    (lambda (t)
+      (map hash->doctest (read-json t)))))
+
+(define (run test-case)
+  (call-with-input-bytes
+   (doctest-MAST test-case)
+   (lambda (mast-port)
+     (printf "~A~A: " (doctest-section test-case) (doctest-lineno test-case))
+     (pretty-print (decode-mast mast-port)))))
+
 
 (call-with-input-file "test/doctest_wm.json"
-  (lambda (t)
-    (let ((suite (json->sjson t)))
-      (for ([test-case suite]) (run-test test-case)))))
-
+  (lambda (input)
+    (for ([test-hash (read-json input)])
+      (run (hash->doctest test-hash)))))
