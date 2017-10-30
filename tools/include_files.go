19c8cd6acf7b3e7fff1b6d98795e32ce1e442a18
Dan Connolly <dckc@madmode.com>
2017-10-30 15:31:38 -0500

    include_files: any extension, package, and bytes

--- tools/include_files.go
+++ tools/include_files.go
@@ -3,25 +3,41 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
+	"unicode"
 )
 
 // Reads all .txt files in the current folder
 // and encodes them as strings literals in textfiles.go
 func main() {
+	ext := os.Args[1]
+	outName := os.Args[2]
+	pkgName := os.Args[3]
+	log.Printf("output: %q package: %q extension; %q\n", outName, pkgName, ext)
+
 	fs, _ := ioutil.ReadDir(".")
-	out, _ := os.Create("textfiles.go")
-	out.Write([]byte("package main \n\nconst (\n"))
+	out, _ := os.Create(outName + ".go")
+	out.Write([]byte("package " + pkgName + "\n\n"))
 	for _, f := range fs {
-		if strings.HasSuffix(f.Name(), ".txt") {
-			out.Write([]byte(strings.TrimSuffix(f.Name(), ".txt") + " = `"))
-			f, _ := os.Open(f.Name())
-			io.Copy(out, f)
-			out.Write([]byte("`\n"))
+		if strings.HasSuffix(f.Name(), ext) {
+			log.Printf("found: %q\n", f.Name())
+			out.Write([]byte("var " + strings.TrimSuffix(f.Name(), ext) + " = []byte{\n\t"))
+			content, _ := ioutil.ReadFile(f.Name())
+			for ix, b := range content {
+				if b < 128 && unicode.IsGraphic(rune(b)) {
+					out.Write([]byte(fmt.Sprintf("'%c', ", b)))
+				} else {
+					out.Write([]byte(fmt.Sprintf("0x%02x, ", b)))
+				}
+				if (ix + 1) % 12 == 0 {
+					out.Write([]byte("\n\t"))
+				}
+			}
+			out.Write([]byte("\n}\n"))
 		}
 	}
-	out.Write([]byte(")\n"))
 }
